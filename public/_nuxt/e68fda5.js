62b023774dd3225083f61a3ae6ceb1c61fe403a7
inu <kevinterry0793@gmail.com>
2021-06-06 17:10:02 +0700

    update

---
+++ public/_nuxt/e68fda5.js
@@ -0,0 +1,64 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [0],
+    {
+        151: function (t, e, o) {
+            "use strict";
+            var n = o(34),
+                component = Object(n.a)(
+                    {},
+                    function () {
+                        var t = this.$createElement,
+                            e = this._self._c || t;
+                        return e("div", [e("Nuxt")], 1);
+                    },
+                    [],
+                    !1,
+                    null,
+                    null,
+                    null
+                );
+            e.a = component.exports;
+        },
+        153: function (t, e, o) {
+            o(154), (t.exports = o(155));
+        },
+        195: function (t, e, o) {
+            var content = o(196);
+            content.__esModule && (content = content.default), "string" == typeof content && (content = [[t.i, content, ""]]), content.locals && (t.exports = content.locals);
+            (0, o(79).default)("12ba2d00", content, !0, { sourceMap: !1 });
+        },
+        196: function (t, e, o) {
+            var n = o(78),
+                r = o(152),
+                l = o(197),
+                c = o(198),
+                f = o(199),
+                d = n(!1),
+                m = r(l),
+                y = r(c),
+                h = r(f);
+            d.push([
+                t.i,
+                '@font-face{font-family:"Lato";src:local("Lato"),url(' +
+                    m +
+                    ') format("truetype");font-weight:400;font-style:normal}@font-face{font-family:"Patrick";src:local("Patrick"),url(' +
+                    y +
+                    ') format("truetype");font-weight:400;font-style:normal}@font-face{font-family:"Quicksand";src:local("Quicksand"),url(' +
+                    h +
+                    ') format("truetype");font-weight:400;font-style:normal}/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent;text-decoration:none}abbr[title]{border-bottom:none;text-decoration:underline;-webkit-text-decoration:underline dotted;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details{display:block}summary{display:list-item}[hidden],template{display:none}body{margin:0;font-family:"Quicksand",sans-serif;font-size:16px;position:relative;overflow-x:hidden}*{box-sizing:border-box}.container{margin:0 auto;max-width:1024px}ul{padding:0;list-style:none}',
+                "",
+            ]),
+                (t.exports = d);
+        },
+        197: function (t, e, o) {
+            t.exports = o.p + "fonts/Lato.9919edf.ttf";
+        },
+        198: function (t, e, o) {
+            t.exports = o.p + "fonts/Patrick.252ca06.ttf";
+        },
+        199: function (t, e, o) {
+            t.exports = o.p + "fonts/Quicksand.cffc279.ttf";
+        },
+    },
+    [[153, 15, 1, 16]],
+]);
